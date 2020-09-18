e20fa3ce8c3dc3e76c57c3516335a48f14e9451a
ayguillo <ayguillo@student.42.fr>
2020-09-18 16:06:13 +0200

    [DEUBG]

--- solve/Al.go
+++ solve/Al.go
@@ -27,28 +27,59 @@ func VerifConclusion(ctx *s.Context, Conclusion string) string{
 	return ""
 }
 
-func ComplexeCase(ctx *s.Context, retByter byte) (string, bool){
-	for i, rule := range(ctx.Rules) {
+func ReturnNewByte(ctx *s.Context, retByter byte ) byte{
+	for _, rule := range(ctx.Rules) {
 		isTrue := RuleIsTrue(string(rule.Premice), ctx.Variables)
 		if !rule.Used && isTrue == s.UNKNOW {
 			for _, byter := range(rule.UsedVar) {
+				fmt.Println("byte", string(byter))
 				if byter != retByter {
 					ctx.Variables[byter] = false
-					ComplexeCase(ctx, byter)
+					fmt.Println(string(byter), "devient faux")
+					return (byter)
 				}
 			}
-		} else if !rule.Used && isTrue == s.TRUE {
+		}
+	}
+	return('1')
+}
+
+func ComplexeCase(ctx *s.Context, retByter byte) (string, bool){
+	newByte := ReturnNewByte(ctx, retByter)
+	if newByte == '1' {
+		fmt.Println("C'est la fin")
+		return "", false
+	}
+	unknow_rule := false
+	for i, rule := range(ctx.Rules) {
+		for key, value := range ctx.Variables {
+			fmt.Println(string(key), value)
+		}
+		isTrue := RuleIsTrue(string(rule.Premice), ctx.Variables)
+		if !rule.Used && isTrue == s.TRUE {
 			if VerifConclusion(ctx, string(rule.Conclusion)) != "" {
+				fmt.Println("Fail conclusion", string(newByte))
 				ctx.Rules[i].Used = false
-				delete(ctx.Variables, retByter)
-				ComplexeCase(ctx, retByter)
+				delete(ctx.Variables, newByte)
+				ComplexeCase(ctx, newByte)
 			} else {
+				fmt.Println("Ok")
+				fmt.Println(rule)
 				ctx.Rules[i].Used = true
-				return "", true
 			}
 		} else if !rule.Used && isTrue == s.FALSE {
+			fmt.Println("False")
+			fmt.Println(rule)
 			ctx.Rules[i].Used = true
+		} else if !rule.Used && isTrue == s.UNKNOW {
+			unknow_rule = true
+			fmt.Println("Unknow", isTrue, rule)
 		}
+		fmt.Println()
+	}
+	if unknow_rule == true {
+		fmt.Println("Et on recommence")
+		ComplexeCase(ctx, newByte)
 	}
 	return "", false
 }
